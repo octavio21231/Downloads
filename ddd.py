783c9d3ab1e2a6ea7b566c83574abaef298fda6f
batman3 <octaviaugustomaciel@gmail.com>
2024-06-27 15:00:03 -0300

    xic

---
+++ ddd.py
@@ -0,0 +1,13 @@
+nomes_a = ["luis", "joao", "Julia", "Jessica"]
+
+nomes_b = lista_a
+
+nomes_b.append("residente_novo")
+
+nomes = nomes_a[:] # ou então nomes_c = nomes_a.copy()
+
+nomes_c.append("residente_novo_donovo")
+
+print(nomes_c)
+
+print(nomes_c)
